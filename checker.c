5b7fd5d3f21786565988da7e2e3af8497474791e
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-08-27 19:16:31 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
  
-int batteryOperatingLimit(float value, float min, float max, const char* message) {
+int isParameterOutOfRange(float value, float min, float max, const char* message) {
     if (value < min || value > max) {
         printf("%s\n", message);
         return 1;
@@ -9,15 +9,17 @@ int batteryOperatingLimit(float value, float min, float max, const char* message
     return 0;
 }
  
-int BatteryWithinLimit(float temperature, float soc, float chargeRate) {
-    int TempRangeLimit = batteryOperatingLimit(temperature, 0, 45, "Temperature out of range!");
-    int SocRangeLimit = batteryOperatingLimit(soc, 20, 80, "State of Charge out of range!");
-    int ChargerateRangeLimit = batteryOperatingLimit(chargeRate, 0, 0.8, "Charge Rate out of range!");
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    int isTempOutOfRange = isParameterOutOfRange(temperature, 0, 45, "Temperature out of range!");
+    int isSocOutOfRange = isParameterOutOfRange(soc, 20, 80, "State of Charge out of range!");
+    int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
  
     return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
 }
  
 int main() {
-    assert(BatteryWithinLimit(25, 70, 0.7));
-    assert(!BatteryWithinLimit(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
 }
+ 
+ 
