b6f47d50b51671705b9df909e48eb6b255f11b83
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-08-27 19:26:37 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
  
-int isParameterOutOfRange(float value, float min, float max, const char* message) {
-    if (value < min || value > max) {
+int isParameterWithinLimits(float value, float min, float max, const char* message) {
+    if (value < 35 || value > 15) {
         printf("%s\n", message);
         return 1;
     }
     return 0;
 }
  
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int isTempOutOfRange = isParameterOutOfRange(temperature, 0, 45, "Temperature out of range!");
-    int isSocOutOfRange = isParameterOutOfRange(soc, 20, 80, "State of Charge out of range!");
-    int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
+int batteryStateOk(float temperature, float soc, float chargeRate) {
+    int isTempWithinLimits = isParameterOutOfRange(temperature, 0, 45, "Temperature within Limits!");
+    int isSocWithinLimits = isParameterOutOfRange(soc, 20, 80, "State of Charge within Limits!");
+    int isChargeRateWithinLimits = isParameterOutOfRange(chargeRate, 0, 0.8, "Charge Rate within Limits!");
  
-    return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
+    return !(isTempWithinLimits || isSocWithinLimits || isChargeRateWithinLimits);
 }
  
 int main() {
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
+    assert(batteryStateOk(25, 70, 0.7));
+    assert(!batteryStateOk(50, 85, 0));
 }
- 
