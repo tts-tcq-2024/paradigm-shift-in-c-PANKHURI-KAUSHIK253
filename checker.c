9f7ee8c5d150463137a6de0ac4abb4398f298b73
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-08-27 14:20:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,10 +4,10 @@
 #define COOLING_THRESHOLD 30          // Temperature threshold for cooling mode (in °C)
 #define HEATING_THRESHOLD 5           // Temperature threshold for heating mode (in °C)
 #define HIGH_COOLING_THRESHOLD 40     // Temperature increase threshold for high cooling mode (in °C)
-
+#define REFERENCE_TEMPERATURE 25
 int batteryOperatingLimit(float temperature ) {
   if (temperature > COOLING_THRESHOLD) {
-        if (temperature > (BASELINE_TEMPERATURE + HIGH_COOLING_THRESHOLD)) {
+        if (temperature > (REFERENCE_TEMPERATURE + HIGH_COOLING_THRESHOLD)) {
             printf("High Cooling Mode: Temperature is very high (%.2f°C). Aggressive cooling required!\n", temperature);
         } else {
             printf("Cooling Mode: Temperature is high (%.2f°C). Cooling system activated.\n", temperature);
