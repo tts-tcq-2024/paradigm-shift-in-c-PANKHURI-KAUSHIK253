8b267d47f3c02ae1199544d1decf05eebba94bb0
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-10-10 12:03:04 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 #include <assert.h>
- 
-int isParameterWithinLimits(float value, float min, float max, const char* message) {
-    if (value < min || value > max) 
-    {
+
+typedef struct {
+    int warningEnabled;  // 1 for enabled, 0 for disabled
+    float min;
+    float max;
+    float warningTolerance;  // Tolerance value for warnings
+} ParameterConfig;
+
+void checkAndPrintWarning(float value, ParameterConfig config, const char* warningMessage) {
+    if (config.warningEnabled) {
+        // Check for warning conditions
+        if (value < config.min + config.warningTolerance) {
+            printf("Warning: Approaching discharge\n");
+        } else if (value > config.max - config.warningTolerance) {
+            printf("Warning: Approaching charge-peak\n");
+        }
+    }
+}
+
+int isParameterOutOfRange(float value, float min, float max, const char* message) {
+    if (value < min || value > max) {
         printf("%s\n", message);
         return 1;
     }
     return 0;
 }
- 
-int batteryStateOk(float temperature, float soc, float chargeRate) {
-    int isTempWithinLimits = isParameterWithinLimits(temperature, 15, 45, "Temperature within Limits!");
-    int isSocWithinLimits = isParameterWithinLimits(soc, 20, 80, "State of Charge within Limits!");
-    int isChargeRateWithinLimits = isParameterWithinLimits(chargeRate, 0, 0.8, "Charge Rate within Limits!");
- 
-    return !(isTempWithinLimits || isSocWithinLimits || isChargeRateWithinLimits);
+
+int batteryIsOk(float temperature, float soc, float chargeRate, ParameterConfig tempConfig, ParameterConfig socConfig, ParameterConfig chargeRateConfig) {
+    checkAndPrintWarning(temperature, tempConfig, "Temperature out of range!");
+    checkAndPrintWarning(soc, socConfig, "State of Charge out of range!");
+    checkAndPrintWarning(chargeRate, chargeRateConfig, "Charge Rate out of range!");
+
+    int isTempOutOfRange = isParameterOutOfRange(temperature, tempConfig.min, tempConfig.max, "Temperature out of range!");
+    int isSocOutOfRange = isParameterOutOfRange(soc, socConfig.min, socConfig.max, "State of Charge out of range!");
+    int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, chargeRateConfig.min, chargeRateConfig.max, "Charge Rate out of range!");
+
+    return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
 }
- 
+
 int main() {
-    assert(batteryStateOk(25, 70, 0.7));
-    assert(!batteryStateOk(50, 85, 0));
+    ParameterConfig temperatureConfig = {1, 0, 45, 0}; // Warning enabled, no tolerance for temperature
+    ParameterConfig socConfig = {1, 20, 80, 4};       // Warning enabled, 5% tolerance for SOC
+    ParameterConfig chargeRateConfig = {1, 0, 0.8, 0}; // Warning enabled, no tolerance for charge rate
+
+    assert(batteryIsOk(25, 70, 0.7, temperatureConfig, socConfig, chargeRateConfig));
+    assert(!batteryIsOk(50, 85, 0, temperatureConfig, socConfig, chargeRateConfig));
+
+    return 0;
 }
