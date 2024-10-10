7680f3e1081e595a281b436de03fbc171e32e9f1
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-10-10 12:24:43 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -37,13 +37,4 @@ int batteryIsOk(float temperature, float soc, float chargeRate, ParameterConfig 
     return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
 }
 
-int main() {
-    ParameterConfig temperatureConfig = {1, 0, 45, 0}; // Warning enabled, no tolerance for temperature
-    ParameterConfig socConfig = {1, 20, 80, 4};       // Warning enabled, 5% tolerance for SOC
-    ParameterConfig chargeRateConfig = {1, 0, 0.8, 0}; // Warning enabled, no tolerance for charge rate
 
-    assert(batteryIsOk(25, 70, 0.7, temperatureConfig, socConfig, chargeRateConfig));
-    assert(!batteryIsOk(50, 85, 0, temperatureConfig, socConfig, chargeRateConfig));
-
-    return 0;
-}
