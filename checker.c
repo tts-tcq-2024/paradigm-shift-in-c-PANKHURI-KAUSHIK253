844d2e7575296acbedcdbd585104cda0cf3b5cf3
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-10-12 11:53:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -27,18 +27,15 @@ int batteryIsOk(float temperature, float soc, float chargeRate, ParameterConfig 
     checkAndPrintWarning(temperature, tempConfig, "Temperature out of range!");
     checkAndPrintWarning(soc, socConfig, "State of Charge out of range!");
     checkAndPrintWarning(chargeRate, chargeRateConfig, "Charge Rate out of range!");
-
     int isTempOutOfRange = isParameterOutOfRange(temperature, tempConfig.min, tempConfig.max, "Temperature out of range!");
     int isSocOutOfRange = isParameterOutOfRange(soc, socConfig.min, socConfig.max, "State of Charge out of range!");
     int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, chargeRateConfig.min, chargeRateConfig.max, "Charge Rate out of range!");
-
     return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
 }
 int main() {
     ParameterConfig temperatureConfig = {1, 0, 45, 0}; // Warning enabled, no tolerance for temperature
     ParameterConfig socConfig = {1, 20, 80, 4};       // Warning enabled, 5% tolerance for SOC
     ParameterConfig chargeRateConfig = {1, 0, 0.8, 0}; // Warning enabled, no tolerance for charge rate
-
     assert(batteryIsOk(25, 70, 0.7, temperatureConfig, socConfig, chargeRateConfig));
     assert(!batteryIsOk(50, 85, 0, temperatureConfig, socConfig, chargeRateConfig));
     return 0;
