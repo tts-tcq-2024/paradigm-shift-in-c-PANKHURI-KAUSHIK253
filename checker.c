95124d5663bda86da7c8de22598957375af10dfc
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-10-31 12:10:28 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
-#include "main.h"
-#include "parameter_config.h"
+typedef struct {
+    int warningEnabled;  // 1 for enabled, 0 for disabled
+    float min;
+    float max;
+    float warningTolerance;  // Tolerance value for warnings
+} ParameterConfig;
 void checkAndPrintWarning(float value, ParameterConfig config, const char* warningMessage) {
     if (config.warningEnabled) {
         // Check for warning conditions
@@ -28,4 +32,14 @@ int batteryIsOk(float temperature, float soc, float chargeRate, ParameterConfig 
     int isSocOutOfRange = isParameterOutOfRange(soc, socConfig.min, socConfig.max, "State of Charge out of range!");
     int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, chargeRateConfig.min, chargeRateConfig.max, "Charge Rate out of range!");
     return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
+}
+int main() 
+{
+    ParameterConfig temperatureConfig = {1, 0, 45, 0}; // Warning enabled, no tolerance for temperature
+    ParameterConfig socConfig = {1, 20, 80, 4};       // Warning enabled, 5% tolerance for SOC
+    ParameterConfig chargeRateConfig = {1, 0, 0.8, 0}; // Warning enabled, no tolerance for charge rate
+   
+    assert(batteryIsOk(25, 70, 0.7, temperatureConfig, socConfig, chargeRateConfig));
+    assert(!batteryIsOk(50, 85, 0, temperatureConfig, socConfig, chargeRateConfig));
+    return 0;
 }
